10ae5456e4de92583f7f024e99d834bf60e5e2b9
Paris-Zidougha <danzid2003@gmail.com>
2025-06-02 17:39:13 +0100

    run

--- src/component/Popup.jsx
+++ src/component/Popup.jsx
@@ -29,7 +29,7 @@ const Popup = ({onClose, whatsappUrl }) => {
            </div>
            <a
         href={whatsappUrl}
-        onClick={() => location.reload()}
+        onClick={() => onClose()}
         target="_blank"
         rel="noopener noreferrer"
         className="bg-green-500 text-white rounded-xl p-3 block mt-4 text-center"
