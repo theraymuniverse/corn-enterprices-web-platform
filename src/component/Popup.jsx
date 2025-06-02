bf0bd5f2547ed9aa408c6bfe2675d6e077a3ca9b
Paris-Zidougha <danzid2003@gmail.com>
2025-06-02 19:55:38 +0100

    gog

--- src/component/Popup.jsx
+++ src/component/Popup.jsx
@@ -23,19 +23,19 @@ const Popup = ({onClose, whatsappUrl }) => {
             <p className='mt-3 text-sm md:text-md text-center '>
                 Our sales representative will reach out to you shortly to confirm your order and offer you a quotation.
             </p>
+                      <a
+                 href={whatsappUrl}
+                 target="_blank"
+                 rel="noopener noreferrer external"
+                 className="bg-green-500 text-[10px] md:text-[15px] text-white rounded-xl p-3 block mt-4 text-center"
+                 onClick={() =>  window.location.reload()}
+               >
+                 Open WhatsApp to complete your order
+               </a>
             <div className='flex flex-row gap-4 justify-center mt-9'>
                <Link to='/'><button onClick={navigate('/')} className='bg-black text-white py-1 px-1 md:px-5 md:py-2 md:text-[15px] rounded-xl text-[10px] hover:bg-gray-500 pointer cursor  duration-300 ease-out'>Return to home page</button></Link>
                <Link to = '/products'><button onClick={navigate('/partner')} className='bg-black text-white py-1 px-1 md:px-5 md:py-2 md:text-[15px] rounded-xl text-[10px] hover:bg-gray-500 pointer cursor duration-300 ease-in-out '> Parnter With Cor'n</button></Link>            
            </div>
-           <a
-        href={whatsappUrl}
-        target="_blank"
-        rel="noopener noreferrer external"
-        className="bg-green-500 text-[10px] md:text-[15px] text-white rounded-xl p-3 block mt-4 text-center"
-        onClick={() =>  window.location.reload()}
-      >
-        Open WhatsApp to complete your order
-      </a>
          </div>
     </div>
     </div>
