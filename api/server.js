584b8a18e4854e5a7a975f5ca405f0b454fd4b27
Paris-Zidougha <danzid2003@gmail.com>
2026-03-28 00:37:23 +0100

    rose

--- api/server.js
+++ api/server.js
@@ -8,7 +8,6 @@ import rateLimit from 'express-rate-limit'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5000
 const resend = new Resend('re_KHFJ9vdH_HT12EFF38X4BQwUQbVn2g7p3')
 
 // ── Middleware ──────────────────────────────────────────────
